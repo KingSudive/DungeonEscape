1098a727595698fe36366ed36ee24575c5649c9f
Saud <wop691@gmail.com>
2026-05-09 11:00:42 +0300

    Add new choice. Some clean up

--- Combat.cpp
+++ Combat.cpp
@@ -3,13 +3,55 @@
 #include <vector>
 #include <string>
 #include <random>
-
+#include <algorithm>
 
 int randomInt(int min, int max)
 {
 	static std::mt19937 rng(std::random_device{}());
 	std::uniform_int_distribution<int> dist(min, max);
 	return dist(rng);
+}
+
+bool playerChoices(Player& player, Enemy& enemy, int input)
+{
+	if (input == 1)
+	{
+		int attackDamage = player.hasItem("Iron Sword") ? randomInt(25, 40) : randomInt(15, 25);
+		std::cout << "\n" "You strike for " << attackDamage << " damage!\n\n";
+		enemy.health -= attackDamage;
+
+		int enemyDamage = randomInt(5, enemy.damage);
+		std::cout << "\nThe skeleton hits you for " << enemyDamage << "!\n";
+		player.health -= enemyDamage;
+	}
+
+	else if (input == 2)
+	{
+		std::cout << "\nThe enemy attacks you, but too weak to hurt you ;)\n\n";
+		int enemyDamage = randomInt(5, enemy.damage);
+		player.health -= enemyDamage / 2;
+	}
+	else if (input == 3)
+	{
+		if (player.hasItem("Health Potion"))
+		{
+			if (player.health + 40 > 100)
+			{
+				player.health = 100;
+			}
+			else
+			{
+				player.health += 40;
+			}
+
+			auto item = std::find(player.inventory.begin(), player.inventory.end(), "Health Potion");
+			if (item != player.inventory.end()) {
+				player.inventory.erase(item);
+			}
+		}
+		std::cout << "You don't have any potions!\n";
+		return false;
+	}
 }
 
 
@@ -24,30 +66,17 @@ bool combat(Player& player, Enemy& enemy)
 		std::cout << "\n-----------------------------\n";
 		std::cout << "What do you want to do?  \n";
 
-		std::cout << "1. Attack \t 2. Defend\n";
+		std::cout << "1. Attack \t 2. Defend \t 3. Use Potion\n";
 
 	
 		std::cin >> input;
 		
-		if (input == 1)
+		bool whatPlayerDecided = playerChoices(player, enemy, input);
+
+		if (!whatPlayerDecided)
 		{
-
-			int attackDamage = player.hasItem("Iron Sword") ? randomInt(25, 40) : randomInt(15, 25);
-			std::cout << "\n" "You strike for " << attackDamage << " damage!\n\n";
-			enemy.health -= attackDamage;
-
-			int enemyDamage = randomInt(5, enemy.damage);
-			std::cout << "The skeleton hits you for " << enemyDamage << "!\n";
-			player.health -= enemyDamage;
+			continue;
 		}
-
-		else if (input == 2)
-		{
-			std::cout << "\nThe enemy attacks you, but too weak to hurt you ;)\n\n";
-			int enemyDamage = randomInt(5, enemy.damage);
-			player.health -= enemyDamage / 2;
-		}
-
 
 		std::cout << enemy.name << " HP: " << enemy.health << "\n";
 		std::cout << player.name << " HP: " << player.health << "\n";
